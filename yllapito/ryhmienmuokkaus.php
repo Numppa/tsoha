9b77cae8fa5933202137053bd2af7b5df0612b7e
Joel <joel@joel-305U1A.(none)>
2013-04-08 15:33:18 +0300

    ryhmälista muokkausta varten

--- yllapito/ryhmienmuokkaus.php
+++ yllapito/ryhmienmuokkaus.php
@@ -5,7 +5,19 @@ onko_kirjautunut(1);
 
 $ryhmat = $kyselija->hae_kaikki_ryhmat();
 
-
+echo '<table border>';
+while ($rivi = $ryhmat->fetch()){
+    echo '<tr>';
+    echo '<td>' . $rivi['id'] . '</td>';
+    echo '<td>' . $rivi['nimi'] . '</td>';
+    
+    echo '<td> <form action="muokkaaryhmaa.php" method="post">';  
+    echo '<input type="submit" name="' . $rivi['id'].'" value="muokkaa"';
+    echo '</form>  </td>';
+    
+    echo '</tr>';
+}
+echo '</table>';
 
 
 ?>
